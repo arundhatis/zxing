0d50a52d57f2fff57c134cc62e22c80b35fcb115
PraJaL55 <trivediprajal@yahoo.co.in>
2017-12-03 13:15:04 -0800

    Resolved test dependencies

--- core/com.google.zxing.test/src/module-info.java
+++ core/com.google.zxing.test/src/module-info.java
@@ -2,4 +2,7 @@ module com.google.zxing.test {
     requires com.google.zxing.aztec;
     requires com.google.zxing.common;
     requires com.google.zxing.pdf417;
+    requires com.google.zxing.readerwriter;
+    requires com.google.zxing.multi;
+    requires com.google.zxing.qrcode;
 }
